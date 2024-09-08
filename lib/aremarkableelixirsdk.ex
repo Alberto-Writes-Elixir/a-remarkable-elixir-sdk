d5c995b4108acc5ccc3a1a145e652e90c239f736
Alberto Hernandez Cerezo <pascu216@gmail.com>
2024-09-08 20:06:07 +0200

    chore: add library files from `mix new` command results.

---
+++ lib/aremarkableelixirsdk.ex
@@ -0,0 +1,18 @@
+defmodule Aremarkableelixirsdk do
+  @moduledoc """
+  Documentation for `Aremarkableelixirsdk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aremarkableelixirsdk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
